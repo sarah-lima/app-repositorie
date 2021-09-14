1669f7d6ef5c2629bcf9e0026e13761ee517ea8e
sarah-lima <sarah.lima@bandtec.com.br>
2021-09-14 20:37:24 -0300

    finish project

--- app/src/main/java/br/com/dio/app/repositories/data/repositories/RepoRepositorieImp.kt
+++ app/src/main/java/br/com/dio/app/repositories/data/repositories/RepoRepositorieImp.kt
@@ -1,11 +1,18 @@
 package br.com.dio.app.repositories.data.repositories
 
+import br.com.dio.app.repositories.core.RemoteException
 import br.com.dio.app.repositories.data.services.GitHubService
 import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
 
 class RepoRepositorieImp(private val service: GitHubService): RepoRepositories {
     override suspend fun listRepositories(user: String) = flow {
-        val repoList = service.listRepositories(user)
-        emit(repoList)
+        try {
+            val repoList = service.listRepositories(user)
+            emit(repoList)
+        }
+        catch (ex: HttpException) {
+            throw RemoteException(ex.message ?: "Não foi possível fazer a busca no moneto!")
+        }
     }
 }
--- app/src/main/java/br/com/dio/app/repositories/ui/MainActivity.kt
+++ app/src/main/java/br/com/dio/app/repositories/ui/MainActivity.kt
@@ -6,13 +6,16 @@ import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
 import br.com.dio.app.repositories.R
+import br.com.dio.app.repositories.core.createDialog
+import br.com.dio.app.repositories.core.createProgressDialog
+import br.com.dio.app.repositories.core.hideSoftKeyboard
 import br.com.dio.app.repositories.databinding.ActivityMainBinding
 import br.com.dio.app.repositories.presentation.MainViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
 
-
+    private val dialog by lazy { createProgressDialog() }
     private val viewModel by viewModel<MainViewModel>()
     private val adapter by lazy { RepoListAdapter() }
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
@@ -29,9 +32,15 @@ class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
 
         viewModel.repos.observe(this) {
             when(it){
-                MainViewModel.State.Loading -> {}
-                is MainViewModel.State.Error -> {}
+                MainViewModel.State.Loading -> dialog.show()
+                is MainViewModel.State.Error -> {
+                    createDialog{
+                        setMessage(it.error.message)
+                    }.show()
+                    dialog.dismiss()
+                }
                 is MainViewModel.State.Success -> {
+                    dialog.dismiss()
                     adapter.submitList(it.list)
                 }
             }
@@ -46,7 +55,8 @@ class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
     }
 
     override fun onQueryTextSubmit(query: String?): Boolean {
-        Log.e(TAG,"onQueryTextSubmit $query")
+        query?.let { viewModel.getRepoList(it) }
+        binding.root.hideSoftKeyboard()
         return true
     }
 
